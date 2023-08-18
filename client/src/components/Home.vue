86883b641b6d6a7e998a22aa7a6986c109bbec43
ariaguirre <ariaguirre64@gmail.com>
2023-08-18 13:52:06 -0300

    carousel

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,19 +1,35 @@
 <template>
-    <div>
-      <div class="header">
-        <h2>Art project title</h2>
-      </div>
-      <button class="btn" @click="goToObjects">Visit virtual museum</button>
+  <div>
+    <div class="header">
+      <h2>Virtual Art Gallery</h2>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    methods: {
-      goToObjects() {
-        this.$router.push('/objects');
-      }
+    <button class="btn" @click="goToObjects">Visit virtual museum</button>
+    <section class="carousel" aria-label="Gallery">
+      <ol class="carousel__viewport">
+  <li id="carousel__slide"
+  tabindex="0"
+  class="carousel__slide">
+  <div class="carousel__snapper">
+  <img src="../img/img2.jpg" alt="Image 2" />
+  <img src="../img/img5.jpg" alt="Image 2" />
+  <img src="../img/img6.jpg" alt="Image 2" />
+  <img src="../img/img7.jpg" alt="Image 2" />
+  <img src="../img/img4.jpg" alt="Image 2" />
+  <img src="../img/img1.jpg" alt="Image 2" />
+</div>
+</li>
+</ol>
+</section>
+</div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goToObjects() {
+      this.$router.push('/objects');
     }
-  };
-  </script>
-    <style scoped src="./Home.css"></style>
+  }
+};
+</script>
+  <style scoped src="./Home.css"></style>
